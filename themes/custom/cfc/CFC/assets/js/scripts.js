dbcfea1f6329f1cd0e811fd8f78ee9bd60837efe
Lahcen Elfarici <lahcen@fornet.ma>
2026-05-19 13:07:50 +0100

    add all commit

--- themes/custom/cfc/CFC/assets/js/scripts.js
+++ themes/custom/cfc/CFC/assets/js/scripts.js
@@ -1942,15 +1942,47 @@
   //   }
   // });
   // Modal Functionality
+  // const modal_1 = $('.all_modale_albume');
+  // const modalVideo = modal_1.find('video');
+
+  // $('.all___class_box_albume').click(function () {
+  //   const videoSrc = $(this).data('video');
+  //   modalVideo.attr('src', videoSrc);
+  //   modal_1.addClass('active');
+
+  //   // Automatically play the video
+  //   modalVideo[0].play();
+  // });
+
+  // $('.close-btn').click(function () {
+  //   modal_1.removeClass('active');
+  //   modalVideo[0].pause();
+  //   modalVideo.attr('src', '');
+  // });
+
+  // modal_1.click(function (e) {
+  //   if ($(e.target).is(modal_1)) {
+  //     modal_1.removeClass('active');
+  //     modalVideo[0].pause();
+  //     modalVideo.attr('src', '');
+  //   }
+  // });
   const modal_1 = $('.all_modale_albume');
   const modalVideo = modal_1.find('video');
 
   $('.all___class_box_albume').click(function () {
     const videoSrc = $(this).data('video');
+
+    // Stop current video first
+    modalVideo[0].pause();
+    modalVideo.attr('src', '');
+
+    // Set new source and open modal
     modalVideo.attr('src', videoSrc);
     modal_1.addClass('active');
 
-    // Automatically play the video
+    // Load then play
+    modalVideo[0].load();
     modalVideo[0].play();
   });
 
